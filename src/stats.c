cc811e1a439f56e4090b655907891704a3931dde
pheonixstorm <darklastat@yahoo.com>
2012-09-02 03:06:04 -0400

    Fixed bug with corrupted stack variable weaponEffect
    Temp fix for annoying weaponEffect corrupted stack bug. Still tracking
    down the fclose.c bug

--- src/stats.c
+++ src/stats.c
@@ -486,7 +486,7 @@ BOOL loadWeaponStats(SBYTE *pWeaponData, UDWORD bufferSize)
 					waterGfx[MAX_NAME_SIZE], muzzleGfx[MAX_NAME_SIZE],
 					trailGfx[MAX_NAME_SIZE], techLevel[MAX_NAME_SIZE];
 	STRING			fireOnMove[10], weaponClass[15], weaponSubClass[15],
-					weaponEffect[15], movement[15], facePlayer[5],
+					weaponEffect[18], movement[15], facePlayer[5],
 					faceInFlight[5],lightWorld[5];
 	UDWORD			longRange, effectSize, numAttackRuns, designable;
 	UDWORD			numRounds;
